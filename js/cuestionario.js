d13db7cbfa5197f5e9732ccd4eb73499a285167f
systemlsm <systemlsm61@gmail.com>
2022-07-12 09:28:58 -0400

    My Love

---
+++ js/cuestionario.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Cuando hablamos por priemra vez?",
+    answer: "Afuera de mi curso",
+    options: [
+      "En las Gradas",
+      "Afuera de mi curso",
+      "Carnavales",
+      "Noche de Museos"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Donde fue nuestro primer beso?",
+    answer: "En las gradas de tu ex cole",
+    options: [
+      "Noche de museos",
+      "En la primera salida",
+      "En las gradas de tu ex cole",
+      "En el colegio"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que color me Gusta?",
+    answer: "Negro, Blanco, rojo y turquesa",
+    options: [
+      "Negro, Amarillo, rojo y turquesa",
+      "Celeste, Blanco, Verde y Plomo",
+      "Blanco, Morado, rojo y turquesa",
+      "Negro, Blanco, rojo y turquesa"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Primera salida donde fuimos?",
+    answer: "Parque urbano",
+    options: [
+      "Multicine",
+      "Parque urbano",
+      "Baticueva",
+      "Prado"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Logros que tuvimos?",
+    answer: "Magia, sistema de ventas, libro y Trabajos",
+    options: [
+      "Magia, sistema de ventas, libro y Trabajos",
+      "Jugar, sistema de ventas y Trabajo",
+      "Magia, sistema y Cuentos",
+      "Magia, sistema de ventas y libro"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
